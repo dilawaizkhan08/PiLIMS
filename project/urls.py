0def8c5cbf98707a4d0572ec8762e0a4d1d4f865
Dilawaiz Khan <dilawaizkhan08@gmail.com>
2025-10-13 15:20:49 +0500

    calc updated

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     
     path("admin/", admin.site.urls),
     path("api/", include("app.urls")),
-    path("events/", include("django_eventstream.urls")),
+    # path("events/", include("django_eventstream.urls")),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
